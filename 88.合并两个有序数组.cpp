82cf694579c972f3a034bc545f20dc8da9e9c173
IrvingW <irvingwang7@gmail.com>
2021-05-04 15:14:30 +0800

    重做原地合并两个有序数组这道题

--- 88.合并两个有序数组.cpp
+++ 88.合并两个有序数组.cpp
@@ -11,18 +11,18 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int index1 = m - 1, index2 = n - 1;
-        int indexMerge = m + n - 1;
-        while (index1 >= 0 || index2 >= 0) {
-            if (index1 < 0) {
-                nums1[indexMerge--] = nums2[index2--];
-            } else if (index2 < 0) {
-                nums1[indexMerge--] = nums1[index1--];
-            } else if (nums1[index1] > nums2[index2]) {
-                nums1[indexMerge--] = nums1[index1--];
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int index = m + n - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[index--] = nums1[p1--];
             } else {
-                nums1[indexMerge--] = nums2[index2--];
+                nums1[index--] = nums2[p2--];
             }
+        }
+        while (p2 >= 0) {
+            nums1[index--] = nums2[p2--];
         }
     }
 };
