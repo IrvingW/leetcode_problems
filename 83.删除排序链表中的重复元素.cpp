c1b5001b7f37a96bbd193079156596b4eb749b9b
tao.wang <tao.wang@yitu-inc.com>
2021-06-20 11:14:42 +0800

    数组原地修改

--- 83.删除排序链表中的重复元素.cpp
+++ 83.删除排序链表中的重复元素.cpp
@@ -18,7 +18,19 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-
+        if (head == nullptr) return nullptr;
+        ListNode* pre = head;
+        ListNode* cur = head->next;
+        while(cur != nullptr) {
+            if (cur->val == pre->val) {
+                pre->next = cur->next;
+                cur = cur->next;
+            } else {
+                pre = cur;
+                cur = cur->next;
+            }
+        }
+        return head;
     }
 };
 // @lc code=end
