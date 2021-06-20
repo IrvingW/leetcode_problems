6d99277dd25ecd7596e0badc0728afdeb34ee044
tao.wang <tao.wang@yitu-inc.com>
2021-06-20 16:19:15 +0800

    旋转链表

---
+++ 61.旋转链表.cpp
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode.cn id=61 lang=cpp
+ *
+ * [61] 旋转链表
+ *
+ * https://leetcode-cn.com/problems/rotate-list/description/
+ *
+ * algorithms
+ * Medium (41.79%)
+ * Likes:    576
+ * Dislikes: 0
+ * Total Accepted:    174K
+ * Total Submissions: 416.4K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给你一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,4,5], k = 2
+ * 输出：[4,5,1,2,3]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：head = [0,1,2], k = 4
+ * 输出：[2,0,1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 链表中节点的数目在范围 [0, 500] 内
+ * -100
+ * 0
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (head == nullptr) return nullptr;
+        int len = 0;
+        ListNode* cur = head;
+        ListNode* tail;
+        while(cur != nullptr) {
+            tail = cur;
+            cur = cur->next;
+            len++;
+        }
+        k = k % len;
+        tail->next = head;
+        k = len - k - 1;
+        for (int i = 0; i < k; i++) {
+            head = head->next;
+        }
+        ListNode* newHead = head->next;
+        head->next = nullptr;
+        return newHead;
+    }
+};
+// @lc code=end
+
